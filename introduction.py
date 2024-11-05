3132934848de4630b10e7adaf4a9cfd8995e3f7d
talish-gaurav <97028189+talish-gaurav@users.noreply.github.com>
2024-11-05 18:48:31 +0000

    Add files via upload

---
+++ introduction.py
@@ -0,0 +1,20 @@
+a=15
+b=9
+c=18
+
+if a>c and a>b:
+
+     print("a is the greatest")
+
+else:
+     
+     print("a is not the greatest")
+
+
+if a>b or a>c:
+
+     print("a is not the smallest value")
+
+else:
+     
+     print("a is the smallest value")
